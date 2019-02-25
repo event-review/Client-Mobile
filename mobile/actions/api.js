99ee9e67af377bb55ee1676282e4857f6e664e75
imanuelvicky <inv484@gmail.com>
2019-02-25 11:57:39 +0700

    file qr code tgal di copas dari test qr  code.js

--- mobile/actions/api.js
+++ mobile/actions/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://192.168.43.142:3000/'
+  baseURL: 'http://192.168.43.195:3000/'
 })
--- mobile/navigators/SwitchNavigator.js
+++ mobile/navigators/SwitchNavigator.js
@@ -2,13 +2,14 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import BottomNav from './BottomNavigator'
 import Home from "../screens/Home";
 import LoginScreen from "../screens/Login";
+import QRScreen from "../screens/TestQRCode";
 
 const SwitchNav = createSwitchNavigator({
   App: BottomNav,
   // Home: Home,
-  Login: LoginScreen
+  Login: LoginScreen,
 }, {
-    initialRouteName: 'Login'
+    initialRouteName: 'App'
   })
 
 export default createAppContainer(SwitchNav)
